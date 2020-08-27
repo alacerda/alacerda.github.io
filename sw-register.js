8095cf9577cbd16e0ec27e74ebba4f157d9c44f3
Alan Lacerda <alacerda@intruderlabs.com.br>
2020-08-27 04:28:30 -0400

    [Automation] Update site files.

--- sw-register.js
+++ sw-register.js
@@ -1,5 +1,5 @@
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('/service-worker.js?v=1598514850').then(function(reg) {
+            navigator.serviceWorker.register('/service-worker.js?v=1598516908').then(function(reg) {
                 reg.onupdatefound = function() {
                     var installingWorker = reg.installing;
                     installingWorker.onstatechange = function() {
