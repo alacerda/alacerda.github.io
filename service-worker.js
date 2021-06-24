9d4b63adcbbc0707f0f48aafbc2ba675ca2a5a36
Alan Lacerda (alacerda) <alancordeiro@gmail.com>
2021-06-24 15:41:11 -0400

    CSS adjustment: Content red 2 Green

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"e8d37d1f9fcc7ace01f0d0ee23a129be"},{"url":"/posts/internal-pentest-tricks/","revision":"79b05d70c6a09cc739971c5f9da19c6b"},{"url":"/posts/o-formato-elf/","revision":"c104dbcbb3de0291f9fdf4751975df99"},{"url":"/posts/word-objetos-com/","revision":"77d73970c7f07b62049c53416fc13b15"},{"url":"/posts/versao-powershell/","revision":"a909219e727f0a2eb83917fc3de42661"},{"url":"/posts/amsi-bypass/","revision":"d14b0b390fdb98c9c43f9d0749dee01a"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"5d43ecc989f8f3dc3ddfab0c62e2059f"},{"url":"/posts/internal-pentest-tricks/","revision":"79b05d70c6a09cc739971c5f9da19c6b"},{"url":"/posts/o-formato-elf/","revision":"c104dbcbb3de0291f9fdf4751975df99"},{"url":"/posts/word-objetos-com/","revision":"77d73970c7f07b62049c53416fc13b15"},{"url":"/posts/versao-powershell/","revision":"a909219e727f0a2eb83917fc3de42661"},{"url":"/posts/amsi-bypass/","revision":"d14b0b390fdb98c9c43f9d0749dee01a"}];
             // service-worker.js
 
 // set names for both precache & runtime cache
