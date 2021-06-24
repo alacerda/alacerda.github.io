79e0582c283f3ce91e6b62120085565827b21da4
Alan Lacerda (alacerda) <alancordeiro@gmail.com>
2021-06-24 15:09:15 -0400

    New AMSI Bypass

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"10b5ea65aae43bcfd9bc9a0ed838eb97"},{"url":"/posts/tricks-powershell/","revision":"84712b67289ea0edd643a4cc69b74db5"},{"url":"/posts/o-formato-elf-copy/","revision":"c104dbcbb3de0291f9fdf4751975df99"},{"url":"/posts/word-objetos-com/","revision":"77d73970c7f07b62049c53416fc13b15"},{"url":"/posts/versao-powershell/","revision":"a909219e727f0a2eb83917fc3de42661"},{"url":"/posts/amsi-bypass/","revision":"47f83da8cc1f1f67ec3af9d6743d4256"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"e8d37d1f9fcc7ace01f0d0ee23a129be"},{"url":"/posts/internal-pentest-tricks/","revision":"79b05d70c6a09cc739971c5f9da19c6b"},{"url":"/posts/o-formato-elf/","revision":"c104dbcbb3de0291f9fdf4751975df99"},{"url":"/posts/word-objetos-com/","revision":"77d73970c7f07b62049c53416fc13b15"},{"url":"/posts/versao-powershell/","revision":"a909219e727f0a2eb83917fc3de42661"},{"url":"/posts/amsi-bypass/","revision":"d14b0b390fdb98c9c43f9d0749dee01a"}];
             // service-worker.js
 
 // set names for both precache & runtime cache
