244f09f6b27500d8e3384af493897056457f51b8
Alan Lacerda (alacerda) <alancordeiro@gmail.com>
2021-06-26 21:31:02 -0400

    SPF: adding references

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
                         importScripts("/assets/js/workbox-v3.6.3/workbox-sw.js");
             workbox.setConfig({modulePathPrefix: "/assets/js/workbox-v3.6.3"});
 
-            self.__precacheManifest = [{"url":"/index.html","revision":"dcf60ff4c73d2d0ca64a1d5057047984"},{"url":"/posts/email-spoof-vs-spf/","revision":"e7f2741c36926dbf1b1515c614d586ee"},{"url":"/posts/internal-pentest-tricks/","revision":"79b05d70c6a09cc739971c5f9da19c6b"},{"url":"/posts/o-formato-elf/","revision":"c104dbcbb3de0291f9fdf4751975df99"},{"url":"/posts/word-objetos-com/","revision":"77d73970c7f07b62049c53416fc13b15"},{"url":"/posts/versao-powershell/","revision":"a909219e727f0a2eb83917fc3de42661"}];
+            self.__precacheManifest = [{"url":"/index.html","revision":"96c9185f2325dfc1382eda6d07756371"},{"url":"/posts/email-spoof-vs-spf/","revision":"9299e8728d7ac1c4f185ab1bffe7d8b9"},{"url":"/posts/internal-pentest-tricks/","revision":"79b05d70c6a09cc739971c5f9da19c6b"},{"url":"/posts/o-formato-elf/","revision":"c104dbcbb3de0291f9fdf4751975df99"},{"url":"/posts/word-objetos-com/","revision":"77d73970c7f07b62049c53416fc13b15"},{"url":"/posts/versao-powershell/","revision":"a909219e727f0a2eb83917fc3de42661"}];
             // service-worker.js
 
 // set names for both precache & runtime cache
